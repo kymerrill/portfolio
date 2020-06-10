944cfbb33a3e55de66a3feba1a92843b031c101c
kymerrill <kymerrill619@gmail.com>
2020-06-10 16:15:53 -0700

    my first portfolio website

---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect
+import csv
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+# @app.route('/blog')
+# def blog():
+#     return 'These are my thoughts on blogs: they suck.'
+
+
+# @app.route('/blog/2020/dogs')
+# def blog2():
+#     return 'This is my dog!'
+
+
+# @app.route('/about.html')
+# def about():
+#     return render_template('about.html')
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/#thankyou')
+    else:
+        return 'something went wrong. try again.'
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database:
+        fieldnames = ['name', 'email', 'message']
+        writer = csv.DictWriter(database, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow(data)
